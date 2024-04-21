10c95fe05bc93a9b62d304bacf3e42c5b64b19f1
Vasco Fernandes <vascocfernandes21@gmail.com>
2024-04-21 18:33:55 +0100

    start windows e linux

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const port = 3000;
+const port = 3001;
 
 const app = express();
 const cors = require('cors'); 
 const pool = require('./src/utils/database');
 
 app.use(cors({
-  origin: ['http://localhost:3001'], // Especificar a origem do frontend
+  origin: ['http://localhost:3000'], // Especificar a origem do frontend
   methods: ['GET', 'POST'], // Permitir métodos GET e POST
   allowedHeaders: ['Content-Type', 'Authorization'], // Permitir cabeçalhos específicos
 }));
