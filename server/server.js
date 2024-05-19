aef53163a8f8483700f96471a989635863192bd1
Vasco Fernandes <vascocfernandes21@gmail.com>
2024-05-19 21:59:17 +0100

    .

--- server/server.js
+++ server/server.js
@@ -4,15 +4,15 @@ require ('dotenv').config();
 
 const app = express();
 const cors = require('cors'); 
-const {connect} = require('./src/utils/database');
+const {ligarLocal} = require('./src/utils/localDatabase');
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://pint-softinsa.vercel.app'], // Adicione a URL do Vercel aqui
+  origin: ['http://localhost:3000', 'https://pint-softinsa.vercel.app'], 
   methods: ['GET', 'POST'], // Permitir métodos GET e POST
   allowedHeaders: ['Content-Type', 'Authorization'], // Permitir cabeçalhos específicos
 }));
 
-connect();
+ligarLocal();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
