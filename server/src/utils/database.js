e18fb11914e35d82e021ff6dad744c8d9dc3f985
Vasco Fernandes <vascocfernandes21@gmail.com>
2024-05-21 11:09:29 +0100

    pg server

--- server/src/utils/database.js
+++ server/src/utils/database.js
@@ -1,9 +1,17 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('pint', 'postgres', 'postgres', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 async function connect() {
