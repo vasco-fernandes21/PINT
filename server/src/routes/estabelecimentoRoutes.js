29bd89c3463a3542ec046e4c2e559f10dd0bd4d1
Vasco Fernandes <vascocfernandes21@gmail.com>
2024-06-15 15:57:30 +0100

    alteração /mobile

--- server/src/routes/estabelecimentoRoutes.js
+++ server/src/routes/estabelecimentoRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const estabelecimentoController = require('../controllers/estabelecimentoController');
-const { uploadEstabelecimentos } = require('../config/multer'); 
+const { uploadEstabelecimentos } = require('../config/multer');
 const auth = require('../middlewares/decodeJWT');
 
 router.get('/', auth, estabelecimentoController.listarEstabelecimentos);
+router.get('/mobile', estabelecimentoController.estabelecimentosMobile); 
 router.get('/:id', estabelecimentoController.estabelecimento_id);
-router.get('/mobile', estabelecimentoController.estabelecimentosMobile);
 router.post('/criar', auth, uploadEstabelecimentos.single('foto'), estabelecimentoController.create);
 router.get('/:id/fotos', estabelecimentoController.getFotoEstabelecimento);
 
