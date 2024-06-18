2438e3070c6e04199d4ea233b71c018ea59b3d8f
Tuturyy <artur.aoliveir@gmail.com>
2024-06-18 15:49:05 +0100

    CalendarioPtRedirect

--- client/src/components/calendario/calendario.js
+++ client/src/components/calendario/calendario.js
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import api from '../api/api'; // Certifique-se de que o caminho para o módulo 'api' está correto
+import api from '../api/api';
+import { useNavigate } from 'react-router-dom'; // Importe o hook useNavigate
+import 'moment/locale/pt';
 
 const localizer = momentLocalizer(moment);
 
 const Calendario = () => {
   const [eventos, setEventos] = useState([]);
+  const navigate = useNavigate(); // Use o hook useNavigate
 
   useEffect(() => {
     const fetchEventosCalendario = async () => {
       try {
         const response = await api.get('/eventos');
         const eventosFormatados = response.data.data.map(evento => ({
+          id: evento.id, // Adicione o id do evento para navegação
           title: evento.titulo,
           start: new Date(`${evento.data}T${evento.hora}`),
           end: new Date(`${evento.data}T${evento.hora}`),
@@ -28,14 +32,27 @@ const Calendario = () => {
     fetchEventosCalendario();
   }, []);
 
+  const handleSelectEvent = (event) => {
+    navigate(`/eventos/${event.id}`); // Navegue para a página do evento usando o id
+  };
+
   return (
-    <div style={{ height: '500px' }}>
+    <div style={{ height: '80vh' }}>
       <Calendar
         localizer={localizer}
         events={eventos}
         startAccessor="start"
         endAccessor="end"
         style={{ height: '100%' }}
+        onSelectEvent={handleSelectEvent} // Adicione o manipulador de evento
+        messages={{
+          next: "Seguinte",
+          previous: "Anterior",
+          today: "Hoje",
+          month: "Mês",
+          week: "Semana",
+          day: "Dia",
+        }}
       />
     </div>
   );
