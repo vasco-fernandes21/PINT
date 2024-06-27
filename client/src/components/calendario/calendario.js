1aa79271b4c820a38c190dfa14e87e928630c75f
Tuturyy <artur.aoliveir@gmail.com>
2024-06-27 17:34:12 +0100

    FiltroCalendarioPorArea

--- client/src/components/calendario/calendario.js
+++ client/src/components/calendario/calendario.js
@@ -1,59 +1,100 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import api from '../api/api';
 import { useNavigate } from 'react-router-dom';
 import 'moment/locale/pt';
+import { Select, MenuItem } from "@mui/material";
 
 const localizer = momentLocalizer(moment);
 
 const Calendario = () => {
   const [eventos, setEventos] = useState([]);
+  const [areas, setAreas] = useState([]);
+  const [areaId, setAreaId] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchEventosCalendario = async () => {
+    const fetchAreas = async () => {
       try {
-        const response = await api.get('/eventos');
+        const response = await api.get('/areas');
+        setAreas(response.data.data);
+      } catch (error) {
+        console.error('Erro ao buscar áreas:', error.response || error.message);
+      }
+    };
+
+    fetchAreas();
+  }, []);
+
+  useEffect(() => {
+    const fetchEventos = async () => {
+      const params = {};
+      if (areaId) {
+        params.areaId = areaId;
+      }
+      console.log('Sending request with params:', params);
+      try {
+        setEventos([]);
+        const response = await api.get('/eventos', { params });
+        console.log('Received response:', response);
         const eventosFormatados = response.data.data.map(evento => ({
           id: evento.id,
           title: evento.titulo,
           start: new Date(`${evento.data}T${evento.hora}`),
           end: new Date(`${evento.data}T${evento.hora}`),
           description: evento.descricao,
+          areaId: evento.areaId,
         }));
         setEventos(eventosFormatados);
       } catch (error) {
-        console.error('Erro ao procurar eventos:', error.response || error.message);
+        console.error('Error fetching eventos:', error.response || error.message);
       }
     };
 
-    fetchEventosCalendario();
-  }, []);
+    fetchEventos();
+  }, [areaId]);
 
   const handleSelectEvent = (event) => {
     navigate(`/eventos/${event.id}`);
   };
 
+  const handleAreaChange = (event) => {
+    setAreaId(event.target.value);
+  };
+
   return (
-    <div style={{ height: '80vh' }}>
-      <Calendar
-        localizer={localizer}
-        events={eventos}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: '100%' }}
-        onSelectEvent={handleSelectEvent} 
-        messages={{
-          next: "Seguinte",
-          previous: "Anterior",
-          today: "Hoje",
-          month: "Mês",
-          week: "Semana",
-          day: "Dia",
-        }}
-      />
+    <div>
+      <Select
+        value={areaId}
+        onChange={handleAreaChange}
+        displayEmpty
+        style={{ marginBottom: 20 }}
+      >
+        <MenuItem value="">Todas as Áreas</MenuItem>
+        {areas.map((area) => (
+          <MenuItem value={area.id} key={area.id}>{area.nome}</MenuItem>
+        ))}
+      </Select>
+      <div style={{ height: '75vh' }}>
+        <Calendar
+          localizer={localizer}
+          events={eventos}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: '100%' }}
+          onSelectEvent={handleSelectEvent}
+          messages={{
+            next: "Seguinte",
+            previous: "Anterior",
+            today: "Hoje",
+            month: "Mês",
+            week: "Semana",
+            day: "Dia",
+          }}
+        />
+      </div>
     </div>
   );
 };
