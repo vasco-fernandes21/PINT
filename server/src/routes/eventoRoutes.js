a6c8b2ed1e38e720a078f95a7d3eb4785ade16b7
Vasco Fernandes <vascocfernandes21@gmail.com>
2024-07-09 23:02:31 +0100

    sem auth no mobile

--- server/src/routes/eventoRoutes.js
+++ server/src/routes/eventoRoutes.js
@@ -10,7 +10,7 @@ router.get('/criados', auth, eventoController.eventosCriador);
 router.get('/validar', auth, eventoController.EventosPorValidar);
 router.put('/validar/:id', auth, eventoController.validarEvento);
 router.post('/mobile', auth, uploadEventos.single('foto'), eventoController.CriarEventoMobile);
-router.get('/mobile', auth, eventoController.eventosMobile);
+router.get('/mobile', eventoController.eventosMobile);
 router.post('/', auth, uploadEventos.single('foto'), eventoController.CriarEvento);
 router.delete('/:id', eventoController.apagarEvento);
 router.get('/:id', eventoController.getEvento);
