45fa925c48ed641dd811bd110fee2d60a5f1979c
Vasco Fernandes <vascocfernandes21@gmail.com>
2024-07-17 02:40:49 +0100

    albuns finalizados (?)

--- client/src/components/album/albumDetail.js
+++ client/src/components/album/albumDetail.js
@@ -1,128 +1,205 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState, useRef } from 'react';
+import { useParams } from 'react-router-dom';
 import {
   Box,
   Typography,
+  Grid,
   Card,
-  CardContent,
   CardMedia,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
+  CardContent,
+  IconButton,
   Button,
   Fab,
-  TextField,
   CircularProgress
-} from "@mui/material";
-import { styled } from "@mui/system";
-import AddIcon from "@mui/icons-material/Add";
-import api from "../api/api";
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
+import api from '../api/api';
+import Swal from 'sweetalert2';
+import EditarAlbum from './albumEditar'; 
 
-function AlbumDetail() {
-  const { albumId } = useParams();
+const AlbumDetails = () => {
+  const { albumId } = useParams(); 
   const [album, setAlbum] = useState(null);
-  const [open, setOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [fotos, setFotos] = useState([]);
+  const [openEditDialog, setOpenEditDialog] = useState(false);
   const [newPhoto, setNewPhoto] = useState(null);
   const [uploading, setUploading] = useState(false);
+  const fileInputRef = useRef(null);
 
-  useEffect(() => {
-    const fetchAlbum = async () => {
-      try {
-        const response = await api.get(`/album/${albumId}`);
-        setAlbum(response.data);
-      } catch (error) {
-        console.error('Error fetching album details:', error);
-      }
-    };
-
-    fetchAlbum();
-  }, [albumId]);
-
-  const handleFileChange = (event) => {
-    setNewPhoto(event.target.files[0]);
-  };
-
-  const handleUploadPhoto = async () => {
-    if (newPhoto) {
-      setUploading(true);
-      const formData = new FormData();
-      formData.append('foto', newPhoto);
-
-      try {
-        await api.post(`/album/${albumId}/fotos`, formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-        setOpen(false);
-        setNewPhoto(null);
-        setUploading(false);
-        // Refetch the album details to show the new photo
-        const response = await api.get(`/album/${albumId}`);
-        setAlbum(response.data);
-      } catch (error) {
-        console.error('Error uploading photo:', error);
-        setUploading(false);
-      }
+  const fetchAlbumDetails = async () => {
+    try {
+      const response = await api.get(`/album/${albumId}`);
+      setAlbum(response.data.data);
+    } catch (error) {
+      console.error('Error fetching album details:', error);
     }
   };
 
+  const fetchAlbumFotos = async () => {
+    try {
+      const response = await api.get(`/album/${albumId}/fotos`);
+      setFotos(response.data.data);
+    } catch (error) {
+      console.error('Error fetching album photos:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAlbumDetails();
+    fetchAlbumFotos();
+  }, [albumId]);
+
+  const handleDelete = async (fotoId) => {
+    try {
+      const result = await Swal.fire({
+        title: 'Você tem certeza?',
+        text: 'Esta ação não pode ser desfeita!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#1D324F',
+        cancelButtonColor: '#6c757d',
+        confirmButtonText: 'Sim, apagar!',
+        cancelButtonText: 'Cancelar',
+      });
+
+      if (result.isConfirmed) {
+        await api.delete(`/album/${fotoId}/fotos`);
+        setFotos(fotos.filter(foto => foto.id !== fotoId));
+        Swal.fire({
+          title: 'Foto apagada!',
+          icon: 'success',
+          confirmButtonColor: '#1D324F',
+        });
+      }
+    } catch (error) {
+      console.error('Erro ao apagar foto:', error);
+      Swal.fire(
+        'Erro!',
+        'Ocorreu um erro ao apagar a foto. Tente novamente.',
+        'error'
+      );
+    }
+  };
+
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setNewPhoto(file);
+      await handleUploadPhoto(file);
+    }
+  };
+
+  const handleUploadPhoto = async (file) => {
+    setUploading(true);
+    const formData = new FormData();
+    formData.append('foto', file);
+
+    try {
+      await api.post(`/album/${albumId}/fotos`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      setNewPhoto(null);
+      fetchAlbumFotos(); // Refetch the album photos
+    } catch (error) {
+      console.error('Erro ao enviar foto:', error);
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  const handleFileInputClick = () => {
+    fileInputRef.current.click();
+  };
+
+  if (!album) {
+    return <Typography variant="h6">A carregar...</Typography>;
+  }
+
   return (
     <Box sx={{ padding: 2 }}>
-      {album ? (
-        <Card>
-          <CardMedia
-            component="img"
-            image={`${process.env.REACT_APP_API_URL}/uploads/albuns/${album.foto}`}
-            alt={album.nome}
-            sx={{ height: 300, objectFit: 'cover' }}
-          />
-          <CardContent>
-            <Typography variant="h5">{album.nome}</Typography>
-            <Typography variant="body2" color="text.secondary">{album.descricao}</Typography>
-          </CardContent>
-        </Card>
-      ) : (
-        <CircularProgress />
-      )}
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
+        <Typography variant="h4" sx={{ fontWeight: 'bold', marginBottom: 0 }}>
+          {album.nome}
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<EditIcon />}
+          sx={{ mb: 2 }}
+          onClick={() => setOpenEditDialog(true)} // Abre o modal de edição
+        >
+          Editar Álbum
+        </Button>
+      </Box>
+      <Typography variant="body1" paragraph>
+       {album.descricao}
+      </Typography>
+      <Grid container spacing={2}>
+        {fotos.map((foto) => (
+          <Grid item xs={12} sm={6} md={4} key={foto.id}>
+            <Card sx={{ position: 'relative' }}>
+              <CardMedia
+                component="img"
+                height="300"
+                image={`${process.env.REACT_APP_API_URL}/uploads/albuns/${foto.foto}`}
+                alt={foto.descricao}
+              />
+              <CardContent>
+                <Typography variant="body2" color="textSecondary">
+                  Enviada por: {foto.criador.nome}
+                </Typography>
+              </CardContent>
+              <IconButton 
+                onClick={() => handleDelete(foto.id)} 
+                sx={{ 
+                  position: 'absolute', 
+                  bottom: 8, 
+                  right: 8, 
+                  color: 'gray' 
+                }}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
 
+      <EditarAlbum
+        open={openEditDialog}
+        handleClose={() => setOpenEditDialog(false)}
+        album={album} 
+        aoEnviar={fetchAlbumDetails}
+      />
+
+      {/* Floating Action Button for uploading photos */}
       <Fab
         aria-label="add"
-        onClick={() => setOpen(true)}
+        onClick={handleFileInputClick}
         style={{ position: 'fixed', bottom: 35, right: 20, backgroundColor: '#1D324F' }}
       >
         <AddIcon style={{ color: '#fff' }} />
       </Fab>
 
-      <Dialog open={open} onClose={() => setOpen(false)} maxWidth="md" fullWidth>
-        <DialogTitle>Upload de Foto</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Selecione uma foto para adicionar ao álbum.
-          </DialogContentText>
-          <Box component="form" noValidate>
-            <TextField
-              type="file"
-              onChange={handleFileChange}
-              fullWidth
-              sx={{ mb: 2 }}
-            />
-            <Button
-              variant="contained"
-              onClick={handleUploadPhoto}
-              color="primary"
-              disabled={uploading}
-              fullWidth
-            >
-              {uploading ? <CircularProgress size={24} /> : 'Upload Foto'}
-            </Button>
-          </Box>
-        </DialogContent>
-      </Dialog>
+      <input
+        ref={fileInputRef}
+        type="file"
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+        accept="image/*"
+      />
+
+      {uploading && (
+        <Box sx={{ mt: 2 }}>
+          <CircularProgress />
+        </Box>
+      )}
     </Box>
   );
-}
+};
 
-export default AlbumDetail;
+export default AlbumDetails;
